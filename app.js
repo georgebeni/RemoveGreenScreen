4d06bc479549d0b86dddb6950c71adbf4d11c291
georgeb98 <74622106+georgeb98@users.noreply.github.com>
2021-06-01 15:53:52 +0300

    Add logic for existing images.

--- app.js
+++ app.js
@@ -1,3 +1,6 @@
+var isCharacter = false;
+var isBackground = false;
+
 var imgCharacter = null;
 var imgBackground = null;
 
@@ -7,6 +10,7 @@ function loadCharacter() {
     var canvas = document.getElementById("character");
     imgCharacter = new SimpleImage(fileInput);
     imgCharacter.drawTo(canvas);
+    isCharacter = true;
 }
 
 // Upload Background
@@ -15,26 +19,31 @@ function loadBackground() {
     var canvas = document.getElementById("background");
     imgBackground = new SimpleImage(fileInput);
     imgBackground.drawTo(canvas);
+    isBackground = true;
 }
 
 // Remove green background and add new background to a new image
 function merge() {
-    var imgResult = document.getElementById("result");
+    if (!isCharacter || !isBackground) {
+        alert("You need to insert both images. Than press 'Merge Images' button!");
+    } else {
+        var imgResult = document.getElementById("result");
 
-    var resultImage = new SimpleImage(imgCharacter.width, imgCharacter.height);
+        var resultImage = new SimpleImage(imgCharacter.width, imgCharacter.height);
 
-    for (var pixel of imgCharacter.values()) {
-        if (pixel.getGreen() > pixel.getRed() + pixel.getBlue()) {
-            var x = pixel.getX();
-            var y = pixel.getY();
-            var newPixel = imgBackground.getPixel(x, y);
-            resultImage.setPixel(x, y, newPixel);
-        } else {
-            resultImage.setPixel(pixel.getX(), pixel.getY(), pixel);
+        for (var pixel of imgCharacter.values()) {
+            if (pixel.getGreen() > pixel.getRed() + pixel.getBlue()) {
+                var x = pixel.getX();
+                var y = pixel.getY();
+                var newPixel = imgBackground.getPixel(x, y);
+                resultImage.setPixel(x, y, newPixel);
+            } else {
+                resultImage.setPixel(pixel.getX(), pixel.getY(), pixel);
+            }
         }
-    }
 
-    resultImage.drawTo(imgResult);
+        resultImage.drawTo(imgResult);
+    }
 }
 
 // Save Image
@@ -43,17 +52,3 @@ async function save(el) {
     const imageURI = canvas.toDataURL("image/jpg");
     el.href = imageURI;
 };
-
-// Reset Canvas Image
-function reset() {
-    var character = document.getElementById("character")
-    var background = document.getElementById("background");
-    var result = document.getElementById("result");
-    
-    var context = character.getContext("2d");
-    context.clearRect(0, 0, character.width, character.height);
-    var context = background.getContext("2d");
-    context.clearRect(0, 0, background.width, background.height);
-    var context = result.getContext("2d");
-    context.clearRect(0, 0, result.width, result.height);
-}
